c391542525381b6c211034aca993311ab1eb28d5
Srikanth-Dubyala <dubyalasrikanth9@gmail.com>
2026-01-27 09:49:21 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -27,7 +27,9 @@ llm = HuggingFaceEndpoint(
     repo_id="mistralai/Mistral-7B-Instruct-v0.2",
     task="text-generation",
     max_new_tokens=3072,
-    temperature=0.1
+    temperature=0.1,
+    huggingfacehub_api_token=HUGGINGFACE_API_KEY
+    
 )
 
 chat_model = ChatHuggingFace(llm=llm)
@@ -476,6 +478,7 @@ def run_contract_analysis(file_path: str):
 
 
 # In[ ]:
+
 
 
 
